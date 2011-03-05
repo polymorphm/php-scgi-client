ff2297e1499efaad30c4364484871a3b21789ef7
Andrej A Antonov <polymorphm@gmail.com>
2011-03-05 19:55:29 +0300

    commention fix

--- SAMPLE.php_scgi_client_conf.php
+++ SAMPLE.php_scgi_client_conf.php
@@ -1,6 +1,6 @@
 <?php
 
 $PHP_SCGI_CLIENT_CONF__CONF = array(
-    # path (url) to socket file:
+    # path (URL) to socket file:
     'SOCKET_FILE' => 'unix://'.realpath(dirname(__FILE__).'/scgi.sock')
 );
