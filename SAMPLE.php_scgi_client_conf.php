35be57b8687f1db904e0b1e878d2c380607ac043
Andrej A Antonov <polymorphm@gmail.com>
2011-03-06 04:19:56 +0300

    fix '#' to '//'

--- SAMPLE.php_scgi_client_conf.php
+++ SAMPLE.php_scgi_client_conf.php
@@ -1,17 +1,17 @@
 <?php
 
 $PHP_SCGI_CLIENT_CONF__CONF = array(
-    # path (URL) to socket file:
+    // path (URL) to socket file:
     'SOCKET_FILE' => 'unix://'.realpath(dirname(__FILE__).'/scgi.sock')
     
-    # automatic start SCGI-daemon when first HTTP-request:
-    #'SCGI_DAEMON_AUTO_START' => FALSE,
+    // automatic start SCGI-daemon when first HTTP-request:
+    //'SCGI_DAEMON_AUTO_START' => FALSE,
     
-    # command line for SCGI-daemon start:
-    #   (do not forget to write char '&' at the end of command, if necessary)
-    #'SCGI_DAEMON_START_CMD' => ...,
+    // command line for SCGI-daemon start:
+    //   (do not forget to write char '&' at the end of command, if necessary)
+    //'SCGI_DAEMON_START_CMD' => ...,
     
-    # number of microseconds delay, after SCGI-daemon start,
-    # before HTTP-request processing:
-    #'SCGI_DAEMON_START_SLEEP' => 3000,
+    // number of microseconds delay, after SCGI-daemon start,
+    // before HTTP-request processing:
+    //'SCGI_DAEMON_START_SLEEP' => 3000,
 );
