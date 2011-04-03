2a17154c288f99270a758d668092fbd3d65c0b92
Andrej A Antonov <polymorphm@gmail.com>
2011-04-03 19:09:45 +0400

    licence headers tiny fixes

--- php_scgi_client.php
+++ php_scgi_client.php
@@ -1,5 +1,6 @@
 <?php
-
+// -*- mode: php; coding: utf-8 -*-
+//
 // Copyright 2011 Andrej A Antonov <polymorphm@gmail.com>
 //
 // This program is free software: you can redistribute it and/or modify
