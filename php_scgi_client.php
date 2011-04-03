582af0439eed8a46e053d74295a95eec966b77e2
Andrej A Antonov <polymorphm@gmail.com>
2011-04-03 18:58:10 +0400

    licence headers fixes

--- php_scgi_client.php
+++ php_scgi_client.php
@@ -1,20 +1,19 @@
 <?php
 
-// This file is part of "PHP SCGI Client"
-// (see <https://github.com/2011-03-04-php-scgi-client/php-scgi-client>).
+// Copyright 2011 Andrej A Antonov <polymorphm@gmail.com>
 //
-// "PHP SCGI Client" is free software: you can redistribute it and/or modify
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// "PHP SCGI Client" is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with "PHP SCGI Client".  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 $PHP_SCGI_CLIENT__DEFAULT_CONF = array(
     'SOCKET_FILE' => NULL,
